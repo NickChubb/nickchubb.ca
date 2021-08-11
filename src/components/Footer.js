409f8fc3fc1beb7c4e541b9603d151dc2d6b86e9
Nick Chubb <NickChubb@cs.sfu.ca>
2021-08-11 16:59:18 -0700

    Update footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,14 @@ const Footer = ( position ) => (
         background: `rgb(28,28,28)`,
         backgroundImage: `url(${background})`,
         marginTop: 50,
+        paddingTop: 10,
         boxShadow: '-2px -1px 5px rgba(15,15,15,.3)',
         textAlign: '-webkit-center'
       }}>
         <Menu></Menu>
+        <hr />
+        <small><i>Copyright Nick Chubb &copy; 2021, Built with Gatsby</i></small>
+        <br /><br></br>
     </div>
 )
 
