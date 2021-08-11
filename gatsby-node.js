21e4fdc89a64c784037dee743dafc7a5640d6f05
Nick Chubb <NickChubb@cs.sfu.ca>
2021-08-10 23:57:51 -0700

    Add express server.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// Serve static public directory
+app.use(express.static('public/'))
+
+app.listen(port, () => {
+  console.log(`nickchubb.ca listening at http://localhost:${port}`)
+})
