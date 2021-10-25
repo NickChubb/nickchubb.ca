3de36d8226a87c9ce16ff3d00d70c0bc6f71e25f
Nick Chubb <NickChubb@cs.sfu.ca>
2021-10-24 19:16:50 -0700

    Fix books.

--- src/components/bookshelf/Book.js
+++ src/components/bookshelf/Book.js
@@ -5,15 +5,15 @@ import { BsLink45Deg  } from 'react-icons/bs';
 
 const Book = ({ book, key }) => (   
     <li className="links-menu">
-        <a href={book.link} target="_blank" >
-            <b>{book.title}</b> <i>by</i> {book.author} <small style={{ color: 'seagreen'}}><BsLink45Deg /></small>
+        <a href={book.Link} target="_blank" >
+            <b>{book.Title}</b> <i>by</i> {ook.Author} <small style={{ color: 'seagreen'}}><BsLink45Deg /></small>
         </a>
         <br />
         {
             // If book has notes display BookNotes component
-            book.notes && book.notes.length > 0 ?
+            book.Notes && book.Notes.length > 0 ?
                 
-                <BookNotes notes={book.notes}/>
+                <BookNotes notes={book.Notes}/>
                 :
                 null
         }
