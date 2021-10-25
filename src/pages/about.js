f80c744e95c0463ffb315e20669467d5867975dd
Nick Chubb <NickChubb@cs.sfu.ca>
2021-10-24 18:31:52 -0700

    Add data fetch from backend API.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,33 +4,45 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// Import data
-import bio from '../data/bio.json'
+const About = () => {
+  
+  // Retrieve bio from server
+  const [bio, setBio] = useState([])
+  useEffect(() => {
 
-const About = () => (
-  <Layout>
-    <SEO title="About Me." />
+    const url = `https://nickchubb.ca/api/books`
+    fetch(url)
+      .then(response => response.json())
+      .then(resultData => {
+        setBio(resultData)
+    })
+  })
 
-    <h2>About Me.</h2>
+  return (
+    <Layout>
+      <SEO title="About Me." />
 
-    <div style={{
-      textAlign: 'justify',
-      textJustify: 'inter-word',
-      paddingLeft: 15,
-      paddingRight: 15
-    }}>
-      {
-        bio && bio.length > 0 ?
+      <h2>About Me.</h2>
 
-          bio.map((p, key) => {
-            return <p key={key}>{p}</p>
-          })
-          :
-          <p>Error loading bio</p>
-      }
-    </div>
+      <div style={{
+        textAlign: 'justify',
+        textJustify: 'inter-word',
+        paddingLeft: 15,
+        paddingRight: 15
+      }}>
+        {
+          bio && bio.length > 0 ?
 
-  </Layout>
-)
+            bio.map((p, key) => {
+              return <p key={key}>{p}</p>
+            })
+            :
+            <p>Error loading bio</p>
+        }
+      </div>
+
+    </Layout>
+  )
+}
 
 export default About
